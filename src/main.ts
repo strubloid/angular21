b559e7bce62fc1d3ddfc4185a9cefcba8feeabf6
Strubloid <strubloid@gmail.com>
2026-05-20 12:58:08 +0100

    doing some comments about bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// it is a built in function that bootstraps the Angular application by
+// initializing the root component (App) and applying the specified configuration
+// (appConfig).
+// If there are any errors during the bootstrapping process, they will be caught
+// and logged to the console.
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
