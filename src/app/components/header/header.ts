3e0b9e13020746c1efcd06bddcca4f73d5bb8c9f
Strubloid <strubloid@gmail.com>
2026-05-20 13:17:07 +0100

    adding an interpolation example by changing the value of the father app.ts on header.ts changing the appName variable

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+// Decorator Component to define the header component
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  // this is an example of interpolation in Angular, where
+  // we bind the appName property to the template
+  appName = 'Interpolation in App';
+}
